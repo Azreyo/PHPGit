a9fd6add0cf95185d940f02cce53afabd4cdb184
Azreyo <58790873+Azreyo@users.noreply.github.com>
2026-02-27 20:59:24 +0100

    Add HTTP status display to dev panel with styled response indicators
    Signed-off-by: Azreyo <58790873+Azreyo@users.noreply.github.com>

--- src/includes/dev_panel.php
+++ src/includes/dev_panel.php
@@ -1,1 +1,27 @@
-<?php
+<div class="fixed-bottom bg-dark text-light border border-dark border-2">
+    <?php
+    $status = http_response_code();
+    switch ($status) {
+        case 200:
+            ?>
+            <p class="btn btn-success m-2 p-2">
+                <?php echo $status; ?> OK
+            </p>
+            <?php
+            break;
+        case 400:
+            ?>
+            <p class="btn btn-danger m-2 p-2">
+                <?php echo $status; ?> Bad Request
+            </p>
+            <?php
+            break;
+        default:
+            ?>
+            <p class="btn btn-warning m-2 p-2">
+                <?php echo $status; ?> Unknown Status
+            </p>
+        <?php
+    }
+    ?>
+</div>
