1cdc0b04b65acc5187dbb9865dbce87ff481128b
Azreyo <58790873+Azreyo@users.noreply.github.com>
2026-02-28 21:00:34 +0100

    Add error handling functionality: implement custom error and exception handlers, and include error handler file

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <?php
     require 'config.php';
     $app_env = $_ENV['APP_ENV'] ?? 'app';
+    require __DIR__ . '/includes/error_handler.php';
 
     $page = $_GET['page'] ?? 'home';
     $allowed_pages = ['home', 'about', 'contact', 'explore', '404', '403'];
@@ -30,16 +31,16 @@
     <meta name="keywords" content="git, php">
     <meta name="author" content="Azreyo">
 
-    <?php
-        if($_ENV['APP_ENV'] === 'dev') {
-    ?>
-        <link rel="stylesheet" href="assets/dev.css">
-    <?php
-        }
-    ?>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/style.css">
+    <?php
+    if($_ENV['APP_ENV'] === 'dev') {
+        ?>
+        <link rel="stylesheet" href="assets/dev.css">
+        <?php
+    }
+    ?>
 
 </head>
 <body>
@@ -55,9 +56,10 @@
 
 <?php
     if(isset($app_env) && $app_env === 'dev') {
+
         include 'includes/dev_panel.php';
     }
 ?>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
